a6b7202da6f45b68ce607ea2a73c1ff97733d619
Tim Gabets <tim@gabets.ru>
2017-07-03 15:01:21 +0300

    should be able to convert '13' to 13

--- spec/StringToIntegerSpec.js
+++ spec/StringToIntegerSpec.js
@@ -5,8 +5,24 @@ describe("StringToInteger", function() {
   beforeEach(function() {
     solution = new StringToInteger();
   });
+/*
+  it("should return 0 on empty string", function() {
+    expect(solution.myAtoi('')).toEqual(0);
+  });
 
-  it("should be able to return true", function() {
-    expect(solution.myAtoi('-1')).toEqual(true);
+  it("should return 0 on string containig spaces", function() {
+    expect(solution.myAtoi('     ')).toEqual(0);
+  });
+
+  it("should be able to convert '0' to 0", function() {
+    expect(solution.myAtoi('0')).toEqual(0);
+  });
+*/
+  it("should be able to convert '1' to 1", function() {
+    expect(solution.myAtoi('1')).toEqual(1);
+  });
+
+  it("should be able to convert '13' to 13", function() {
+    expect(solution.myAtoi('13')).toEqual(13);
   });
 });
