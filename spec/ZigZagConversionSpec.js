8d2e06a8ab2dab658f6678ded929fd39161866c5
Tim Gabets <tim@gabets.ru>
2017-07-03 18:05:56 +0300

    Added      lib/ZigZagConversion.js

---
+++ spec/ZigZagConversionSpec.js
@@ -0,0 +1,12 @@
+describe("ZigZagConversion", function() {
+  var ZigZagConversion = require('../lib/ZigZagConversion');
+  var solution;
+
+  beforeEach(function() {
+    solution = new ZigZagConversion();
+  });
+
+  it("should be able to convert string and 1 row", function() {
+    expect(solution.convert('', 1)).toEqual('');
+  });
+});
